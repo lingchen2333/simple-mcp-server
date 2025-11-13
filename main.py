4996f37c058820c7528d8c96c7d8092310738ca0
lingchen <138715540+lingchen2333@users.noreply.github.com>
2025-11-13 00:22:18 +0000

    implement session initialization and agent invocation in main.py

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
 import asyncio
+
 from dotenv import load_dotenv
+from langchain_core.messages import HumanMessage
+from langchain_mcp_adapters.tools import load_mcp_tools
+from langchain_openai import ChatOpenAI
+from mcp import StdioServerParameters, ClientSession
+from mcp.client.stdio import stdio_client
+from langchain.agents import create_agent
 
 load_dotenv()
 
-async def main():
-    print("Hello from simple-mcp-server!")
+llm = ChatOpenAI()
 
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/Users/lingchen/Downloads/langchain_course/simple_mcp_server/servers/math_server.py"],
+)
+
+async def main():
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read, write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+
+            agent = create_agent(llm, tools)
+
+            result = await agent.ainvoke({
+                "messages": [HumanMessage(content="What is 54 + 2* 3?")]
+            })
+
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
